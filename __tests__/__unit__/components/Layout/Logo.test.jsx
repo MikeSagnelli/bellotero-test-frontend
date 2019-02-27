fa2d87a7cf372a64df6184f57a3bb6ef4c8b2fe0
MikeSagnelli <ing.sagnelli@gmail.com>
2019-02-27 09:11:48 -0600

    chore(tests): fix unit tests

--- __tests__/__unit__/components/Layout/Logo.test.jsx
+++ __tests__/__unit__/components/Layout/Logo.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Logo from '../../../../components/Layout/Logo';
 
-describe('Test Logo', () => {
+describe('Logo', () => {
   it('renders logo', () => {
     const component = shallow(<Logo />);
     expect(component).toMatchSnapshot();
